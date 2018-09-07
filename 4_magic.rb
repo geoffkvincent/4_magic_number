8025c4721637f1583ccb0671c29c166b411582b6
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-07 10:28:00 -0600

    brainteaser

---
+++ 4_magic.rb
@@ -0,0 +1,22 @@
+require 'to_words'
+
+def number_words
+  puts "enter number"
+  # num = gets.to_words.length
+  num =gets.to_i
+  num_wrd = num.to_words
+  num_wrd_count = num_wrd.length
+  if num == 4
+    puts '4 is magic number'
+  elsif
+    puts num_wrd_count
+  elsif
+    magic = num_wrd_count.to_words.length
+    puts magic
+  elsif
+    puts magic
+  end 
+end
+
+number_words
+
